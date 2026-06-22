dbf6344f2f107eca1af7dac8a8fea21b00335211
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac.fritz.box>
2026-06-22 14:13:30 +0200

    Elaborazione Dialog AggiungiNuovoFrutto

--- src/app/features/sidenav-content/components/header/components/aggiungi-frutto-dialog/aggiungi-frutto-dialog.ts
+++ src/app/features/sidenav-content/components/header/components/aggiungi-frutto-dialog/aggiungi-frutto-dialog.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { MatDialogTitle, MatDialogClose, MatDialogContent } from "../../../../../../../../node_modules/@angular/material/types/dialog";
+import { Component, inject } from '@angular/core';
+import { MatDialogTitle, MatDialogClose, MatDialogContent } from "@angular/material/dialog";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
+import { NonNullableFormBuilder } from '@angular/forms';
+import { FruitsStore } from '../../../../../../core/store/fruitsStore';
+import { MatFormField, MatInputModule } from "@angular/material/input";
+import {MatSelectModule} from '@angular/material/select'
 
 @Component({
   selector: 'app-aggiungi-frutto-dialog',
-  imports: [MatDialogTitle, MatButtonModule, MatDialogClose, MatIcon, MatDialogContent],
+  imports: [MatDialogTitle, MatButtonModule, MatDialogClose, MatSelectModule, MatIcon, MatDialogContent, MatFormField, MatInputModule],
   templateUrl: './aggiungi-frutto-dialog.html',
   styleUrl: './aggiungi-frutto-dialog.css',
 })
-export class AggiungiFruttoDialog {}
+export class AggiungiFruttoDialog {
+  // Iniettiamo NonNullableFormBuilder per non dichiarare in continuazione
+  // che i valori protrebbero essere null e automaticamente resettare i campi
+  formBuilder = inject(NonNullableFormBuilder);
+  // Iniettiamo lo store per poterlo utilizare
+  fruitsStore = inject(FruitsStore)
+
+  // Istanziamo i valori dello store
+  readonly famiglieDisponibili = this.fruitsStore.famiglieDisponibili
+  readonly ordiniDisponibili = this.fruitsStore.ordiniDisponibili
+  readonly generiDisponibili = this.fruitsStore.generiDisponibili
+
+  // Dichiariamo che il form ha determinati campi,
+  // I Validatori si accertano che siano validi e required che  sono obbligatori
+  aggiungiFruttoForm = this.formBuilder.group({
+
+  })
+}
