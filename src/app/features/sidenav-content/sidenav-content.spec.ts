db6b59d7120de4b9cdd2a173237ab9a2c3886b87
Christian Giaccardi <chrigiaccardi@gmail.com>
2026-07-02 22:36:01 +0200

    Elaborazione Testing App
    Sto cercando di capire le factoryMock per creare un mock base condivisibile tra i test e poi inserire in ogni test quello che manca

--- src/app/features/sidenav-content/sidenav-content.spec.ts
+++ src/app/features/sidenav-content/sidenav-content.spec.ts
@@ -1,21 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
 import { SidenavContent } from './sidenav-content';
 import { FruitsStore } from '../../core/store/fruitsStore';
+
 
 
 describe('SidenavContent', () => {
   let component: SidenavContent;
   let fixture: ComponentFixture<SidenavContent>;
   // Istanziamo lo store
-  let fruitsStoreMock:{}
+  let fruitsStoreMock: {}
+
+  @Component({
+    selector: 'app-header',
+    template: ''
+  })
+  class mockHeader { }
+  
+  @Component({
+    selector: 'app-macedonia-card',
+    template: ''
+  })
+  class mockMacedoniaCard { }
+  
+  @Component({
+    selector: 'app-lista-frutti',
+    template: ''
+  })
+  class mockListaFruttiCard {}
 
   beforeEach(async () => {
     // Dichiariamo lo store
     fruitsStoreMock = {}
 
     await TestBed.configureTestingModule({
-      imports: [SidenavContent],
+      imports: [
+        SidenavContent,
+        mockMacedoniaCard,
+        mockHeader,
+        mockListaFruttiCard
+      ],
       providers: [
         { provide: FruitsStore, useValue: fruitsStoreMock }
       ]
